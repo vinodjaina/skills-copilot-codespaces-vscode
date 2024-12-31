9a2e4792781a2ddef5d89afc63931bfca41bf79b
vinodjaina <jaina.kumar@ecanarys.com>
2024-12-31 06:13:21 +0000

    copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        age: 30,
+        skills: ['JS', 'CSS', 'HTML']
+    };
+    console.log(member.name + ' has ' + member.skills);
+}
